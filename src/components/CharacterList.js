6bf2bf65e73b0dc1a1ec5b604bdc59c1b2f00268
Connor R. Sullivan <crsullivan@protonmail.com>
2019-09-20 11:00:53 -0700

    MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,68 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import {Link} from "react-router-dom";
+import SearchForm from './SearchForm'
+import styled from 'styled-components';
 
-export default function CharacterList() {
+const CharacterListCard = styled.section `
+display:flex;
+flex-wrap:wrap;
+text-align:center;
+margin-left:10%;
+margin-right:10%;
+`
+
+const H2 = styled.h2 `
+margin:2%;
+`
+const CharacterDiv = styled.div `
+background-color:lightgrey;
+margin:2%;
+width:20%;
+`
+const H1 = styled.h1 `
+text-align:center;
+`
+
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([])
+  
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then(response => {
+          const characters = response.data.results;
+          setCharacters(characters);
+          console.log(characters)
+        })
+        .catch(error => {
+          console.log('Server Error', error);
+        });
+    }
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    getCharacters()
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <React.Fragment>
+      <SearchForm />
+      <H1>Check Everyone</H1>
+      <CharacterListCard>
+        
+      
+        {characters.map( character => (
+          <CharacterDiv key={character.id}>
+            <H2>{character.name}</H2>
+            <p>{character.status}</p>
+          </CharacterDiv>
+        ))}
+      </CharacterListCard>
+      </React.Fragment>
   );
 }
+
+export default CharacterList;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+import styled from 'styled-components';
 
-export default function SearchForm() {
- 
+const SearchFormCard = styled.section `
+
+display:flex;
+flex-wrap:wrap;
+`
+const H2 = styled.h2 `
+margin:2%;
+`
+
+const Form = styled.form `
+
+`
+const SearchForm = () => {
+
+  const [searches, setSearches] = useState('');
+  const [searchResult, setSearchResult] = useState([])
+
+  useEffect ( () => {
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+      const peeps = response.data.results
+      const results = peeps.filter(item => {
+        if(item.name.toLowerCase().includes(searches)) {
+        return item.name.toLowerCase().includes(searches)
+      }})
+      setSearchResult(results)
+    })
+  },[searches])
+
+  const handleChange = search => {
+    setSearches(search.target.value)
+  }
+
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <SearchFormCard>
+      <Form>
+        <input type="text" placeholder="Find Quickly" name="searchText" value={searches} onChange={handleChange} />
+
+      </Form>
+      {searchResult.map(item => {
+        return (
+          <div key={item.id}>
+            <Link to = {`/character/${item.id}`}>
+              <H2>{item.name}</H2>
+              <p>{item.status}</p>
+            </Link>
+          </div>
+        )
+      })}
+    </SearchFormCard>
   );
 }
+
+export default SearchForm
